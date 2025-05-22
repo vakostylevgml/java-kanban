18bd6aa859e8de9bccd5787963a4894ee7b84eb7
Vladislav Kostylev <kostylev.v@gmail.com>
2025-05-22 23:11:02 +0300

    feat: HistoryManagerTest

--- test/model/HistoryManagerTest.java
+++ test/model/HistoryManagerTest.java
@@ -3,15 +3,20 @@ package model;
 import manager.Managers;
 import manager.TaskManager;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class HistoryManagerTest {
     static TaskManager manager;
 
-    @BeforeAll
-    static void setUpBeforeClass() {
+    @BeforeEach
+    void beforeEach() {
         manager = Managers.getDefault();
+    }
+
+    @Test
+    void historyIsEmptyByDefault() {
+        Assertions.assertEquals(0, manager.getHistoryManager().getHistory().size());
     }
 
     @Test
@@ -20,5 +25,31 @@ public class HistoryManagerTest {
         long expectedId = manager.create(task);
         manager.findTaskById(expectedId);
         Assertions.assertEquals(expectedId, manager.getHistoryManager().getHistory().getFirst().getId());
+    }
+
+    @Test
+    void tenTasksAreAddedToHistory() {
+        Long[] expectedIds = new Long[10];
+        for (int i = 0; i < 10; i++) {
+            Task task = new Task("a" + i, "b", Status.NEW);
+            long expectedId = manager.create(task);
+            manager.findTaskById(expectedId);
+            expectedIds[i] = expectedId;
+        }
+        Long[] actualIds = manager.getHistoryManager().getHistory()
+                .stream()
+                .map(Task::getId)
+                .toArray(Long[]::new);
+        Assertions.assertArrayEquals(expectedIds, actualIds);
+    }
+
+    @Test
+    void elevenTasksAreNotAddedToHistory() {
+        for (int i = 0; i < 11; i++) {
+            Task task = new Task("a" + i, "b", Status.NEW);
+            long expectedId = manager.create(task);
+            manager.findTaskById(expectedId);
+        }
+        Assertions.assertEquals(10, manager.getHistoryManager().getHistory().size());
     }
 }
